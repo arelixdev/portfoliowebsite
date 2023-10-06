68b5c528f8c692de4e10aa44d05513ab89110c40
BRARD Clément <brard.clement.83@gmail.com>
2023-10-06 11:42:41 +0200

    finish modal system

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ const openModal = () => {
 function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
